6d594c101ae88061a5a51f0266845b2b6ae850eb
Ivan <i.a.konuhov@gmail.com>
2023-07-26 00:21:30 +0700

    Initial Commit

---
+++ Nevermind/NevermindApp.swift
@@ -0,0 +1,17 @@
+//
+//  NevermindApp.swift
+//  Nevermind
+//
+//  Created by Иван on 26.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct NevermindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
